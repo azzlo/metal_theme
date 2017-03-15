b5b7137bb1fac29e7fd3ea2ac00683f3d904dca1
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2017-03-15 17:03:11 -0600

    imagenes para los servicios

--- pageServicios.php
+++ pageServicios.php
@@ -29,18 +29,24 @@
         <div class="col-md-6">
           <h3>Baikal</h3>
           <br>
-          <p>
-            PRENSA DE CORTINA  CNC
-            DE 350 ton. Mesa de long. 3000mm
-          </p>
-          <img src="<?php bloginfo(stylesheet_directory)?>/img/baikal.JPG" alt="" class="imgServicio" style="width:100%; height:auto; border-radius:5px;">
+          <figure>
+            <img src="<?php bloginfo(stylesheet_directory)?>/img/baikal.JPG" alt="" class="imgServicio" style="width:100%; height:auto; border-radius:5px;">
+            <figcaption>  PRENSA DE CORTINA  CNC
+              DE 350 ton. Mesa de long. 3000mm</figcaption>
+          </figure>
+
         </div>
         <div class="col-md-6">
           <h3>Durman</h3>
-          <p>
-            Dos prensas de cortina CNC de 400 Ton. En tandem 8.600 Mts. De longitud total
-          </p>
-          <img src="<?php bloginfo(stylesheet_directory)?>/img/durman.jpg" alt="" class="imgServicio" style="width:100%; height:auto; border-radius:5px;">
+          <br>
+          <figure><img src="<?php bloginfo(stylesheet_directory)?>/img/durman.jpg" alt="" class="imgServicio" style="width:100%; height:auto; border-radius:5px;">
+          <figcaption>Dos prensas de cortina CNC de 400 Ton. En tandem 8.600 Mts. De longitud total</figcaption></figure>
+        </div>
+        <div class="col-md-6">
+          <h3>Convencional</h3>
+          <br>
+          <figure><img src="<?php bloginfo(stylesheet_directory)?>/img/convencional.JPG" alt="" class="imgServicio" style="width:100%; height:auto; border-radius:5px;">
+          <figcaption>Prensa de cortina de 80 ton.  mecánica 2 mts. Longitud bancada</figcaption></figure>
         </div>
       </div>
     </div>
@@ -120,13 +126,57 @@
         </div>
         <div class="col-md-6">
           <h3 class="text-center">Fresadora Vertical</h3>
-          <p>????</p>
-          <img src="<?php bloginfo(stylesheet_directory)?>/img/fresa2.JPG" alt="" class="imgServicio" style="width:100%; height:auto; border-radius:5px;">
+
+          <figure><img src="<?php bloginfo(stylesheet_directory)?>/img/fresa2.JPG" alt="" class="imgServicio" style="width:100%; height:auto; border-radius:5px;">
+          <figcaption>1700x500x400 Hidraulica de alta produccion (copiadora)</figcaption></figure>
         </div>
         <div class="col-md-6">
+          <h3 class="text-center">Fresadora Cabezal Universal</h3>
 
+          <figure><img src="<?php bloginfo(stylesheet_directory)?>/img/fresa1.JPG" alt="" class="imgServicio" style="width:100%; height:auto; border-radius:5px;">
+          <figcaption>Agregar movimientos automaticos y capacidades XYZ</figcaption></figure>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-12 service-container">
+      <div class="row">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2 class="text-center service-title">Soldadura</h2>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <h3 class="text-center">Soldadora arco sumergido</h3>
 
+          <figure><img src="<?php bloginfo(stylesheet_directory)?>/img/soldadora.jpg" alt="" class="imgServicio" style="width:100%; height:auto; border-radius:5px;">
+          <figcaption>?????</figcaption></figure>
+        </div>
+        <div class="col-md-6">
+          <h3 class="text-center">Maquinas Soldadoras</h3>
 
+          <figure><img src="<?php bloginfo(stylesheet_directory)?>/img/maquinaSoldadora.jpg" alt="" class="imgServicio" style="width:100%; height:auto; border-radius:5px;">
+          <figcaption>8 Maquinas soldadoras 1 maquina p/bicelado</figcaption></figure>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-12 service-container">
+      <div class="row">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2 class="text-center service-title">Areas de trabajo</h2>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <h3 class="text-center">Aréa de inspección</h3>
+
+          <figure><img src="<?php bloginfo(stylesheet_directory)?>/img/area1.jpg" alt="" class="imgServicio" style="width:100%; height:auto; border-radius:5px;">
+          <figcaption>Area de inspeccion con mesa de marmol e instrumentos de medicion</figcaption></figure>
+        </div>
+        <div class="col-md-6">
+          <h3 class="text-center">Aréa de pintura</h3>
+
+          <figure><img src="<?php bloginfo(stylesheet_directory)?>/img/area2.jpg" alt="" class="imgServicio" style="width:100%; height:auto; border-radius:5px;">
+          <figcaption>????</figcaption></figure>
         </div>
       </div>
     </div>
