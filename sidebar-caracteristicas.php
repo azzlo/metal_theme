ad96b41557af6012e440e26ecd2f1e84e7170985
Oscar Tellez <oscartzgz@gmail.com>
2017-03-15 10:47:38 -0600

    Trabajo frontend en el index

--- sidebar-caracteristicas.php
+++ sidebar-caracteristicas.php
@@ -1,34 +1,31 @@
 <section id="containerAcercaDe">
   <div class="container">
-
-      <div class="row indexServicios">
-        <div class="filtro">
-          <div class="col-md-6 tituloAlineado">
-            <h3 class="text-center tituloIndex">Servicios</h3>
-          </div>
-          <div class="col-md-6">
-            <p class="parrafoIndex font-big">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-
-
-          </div>
+    <div class="row indexServicios">
+      <div class="filtro">
+        <div class="col-md-6 tituloAlineado">
+          <a href="servicios"><h3 class="text-center tituloIndex">Servicio de Calidad</h3></a>
+        </div>
+        <div class="col-md-6 flex-center">
+          <p class="parrafoIndex font-big">
+            Utilizamos herramientas de ultima tecnologia con el fin de satisfacer las expectativas de nuestros clientes y ofrecer siempre un producto de calidad.
+          </p>
         </div>
       </div>
+    </div>
+    <div class="row indexNosotros">
+      <div class="filtro">
+        <div class="col-md-6 flex-center ">
+          <p class="parrafoIndex font-big">
+            En JEMAC estamos creciendo y por lo tanto buscamos gente apasionada por su trabajo, gente como tú. Por esto hemos designado una sección de vacantes.
+          </p>
+        </div>
+        <div class="col-md-6 tituloAlineado">
+          <a href="vacantes"><h3 class="text-center tituloIndex">Unete a nuestro equipo de trabajo</h3></a>
+        </div>
+      </div>
+    </div>
 
-            <div class="row indexNosotros">
-              <div class="filtro">
-                <div class="col-md-6 ">
-                  <p class="parrafoIndex font-big">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
 
-
-                </div>
-                <div class="col-md-6 tituloAlineado">
-                  <h3 class="text-center tituloIndex">Nosotros</h3>
-                </div>
-
-              </div>
-            </div>
-
-              
 
 
 
--- sidebar-productos.php
+++ sidebar-productos.php
@@ -1,43 +1,26 @@
 <section id="containerProducto" class="container">
   <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-6">
       <div class="seccionProducto">
         <div id="imagen" class="imagen1">
          <div id="info">
-           <p id="headline">Título</p>
-           <p id="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente voluptates.</p>
+           <p id="descripcion">JEMAC no seria posible sin el mejor personal, nuestros ingenieros y trabajadores siempre esta en constante capacitación para brindarte el mejor servicio.</p>
          </div>
        </div>
-        <h3 >Título</h3>
+        <h3 >Personal Capacitado</h3>
       </div>
     </div>
-    <div class="col-md-4">
-
+    <div class="col-sm-12 col-md-6">
         <div class="seccionProducto">
           <div id="imagen" class="imagen2">
            <div id="info">
-             <p id="headline">Título</p>
-             <p id="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente voluptates.</p>
+             <p id="descripcion">Industrias JEMAC a través del área de desarrollo e ingeniería se ofrecen los servicios de asesoramiento técnico para desarrollo de productos de clientes externos y garantizarles el proceso del desarrollo de sus piezas.</p>
            </div>
          </div>
-          <h3 >Título</h3>
+          <h3 >Asesoramiento Técnico</h3>
         </div>
     </div>
-    <div class="col-md-4">
-      <div class="seccionProducto">
-        <div id="imagen" class="imagen3">
-         <div id="info">
-           <p id="headline">Título</p>
-           <p id="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente voluptates.</p>
-         </div>
-       </div>
-       <h3 >Título</h3>
-      </div>
-
-    </div>
   </div>
-
-
 </section>
 <br>
 <br>
