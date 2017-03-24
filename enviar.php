cfecfaebc1796392a5dc335e07bfefda74ea6291
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2017-03-24 12:55:23 -0600

    actualizacion

--- enviar.php
+++ enviar.php
@@ -7,6 +7,6 @@ $telefono= $_POST["Telefono"];
 $mensaje= $_POST["Mensaje"];
 $contenido= "Nombre:  " . $nombre . "\nCorreo:  " . $correo . "\nTelefono:  " . $telefono . "\nMensaje:  " . $mensaje;
 mail($destino, "Contacto", $contenido);
-header("Location:http:http://industriajemac.com.mx/")
+header("Location:http://industriajemac.com.mx/mensaje-enviado/")
 
  ?>
